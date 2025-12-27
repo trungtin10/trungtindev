12117f80d7f4216f3a381237a0c2a42a4871eb03
Nguyen Hin <tintran2k4@gmail.com>
2025-12-27 13:27:02 +0700

    nội dung thay đổi ở đây

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',      // Bắt buộc
+  trailingSlash: true,   // Rất quan trọng: Giúp GitHub hiểu các đường dẫn như /about/ hay /contact/
+  images: {
+    unoptimized: true,   // Bắt buộc để hiển thị được ảnh trên GitHub Pages
+  },
+  // Nếu bạn dùng repo trungtin10.github.io thì KHÔNG cần basePath. 
+  // Nếu dùng repo khác (như /my-blog) thì mới cần thêm basePath: '/my-blog'.
 };
 
 export default nextConfig;
